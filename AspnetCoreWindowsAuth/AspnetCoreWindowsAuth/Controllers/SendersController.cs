315492c475d4ae6ed2a069533deb5e97324a506b
Miranda Rompoti <miranda.rompoti@lexisnexis.co.uk>
2018-06-22 17:23:02 +0100

    GetAndPost

---
+++ AspnetCoreWindowsAuth/AspnetCoreWindowsAuth/Controllers/SendersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetCoreWindowsAuth.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreWindowsAuth.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    public class SendersController : Controller
+    {
+        [HttpGet]
+        public string GetName (Members members)
+        {
+
+            return members.FirstName + " " + members.LastName;
+            
+
+        }
+
+        [HttpPost]
+        public string PostName([FromBody] Members members, string id)
+        {
+            return members.FirstName + " " + members.LastName;
+        }
+    }
+}

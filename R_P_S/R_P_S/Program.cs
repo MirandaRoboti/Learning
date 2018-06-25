ea3e2db123bc8373ac6b6c5e9bd2d40b1faaf89a
Miranda Rompoti <miranda.rompoti@lexisnexis.co.uk>
2018-06-25 14:42:27 +0100

    Divided logic into different classes. Need to add  do while

--- R_P_S/R_P_S/Program.cs
+++ R_P_S/R_P_S/Program.cs
@@ -8,98 +8,15 @@ namespace R_P_S
 {
     class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-          
-            {
-                Console.WriteLine("Choose between rock, paper or scissors...");
-                string userChoice = Console.ReadLine();
+            
+                Console.WriteLine("Do you choose rock,paper or scissors");
+                GameLogic gameLogic = new GameLogic();
+                string userChoise = Console.ReadLine();
+                gameLogic.Game(userChoise);
 
-                Random r = new Random();
-                int computerChoise = r.Next(4);
-
-                if (computerChoise == 1)
-                {
-                    if (userChoice == "rock")
-                    {
-                        Console.WriteLine("The computer chose rock");
-                        Console.WriteLine("Its a tie");
-                    }
-
-                    else if (userChoice == "paper")
-                    {
-                        Console.WriteLine("The computer chose paper");
-                        Console.WriteLine("Its a tie");
-                    }
-
-                    else if (userChoice == "scissors")
-                    {
-                        Console.WriteLine("The computer chose scissors");
-                        Console.WriteLine("Its a tie");
-                    }
-
-                    else Console.WriteLine("Yoy must choose between rock, paper or scissors...");
-
-                }
-
-                else if (computerChoise == 2)
-                {
-                    if (userChoice == "rock")
-                    {
-                        Console.WriteLine("The computer chose paper");
-                        Console.WriteLine("Sorry you lose,paper beat rock");
-
-                    }
-
-                    else if (userChoice == "paper")
-                    {
-                        Console.WriteLine("The computer chose scissors");
-                        Console.WriteLine("Sorry you lose,scissors beat paper");
-
-                    }
-
-
-                    else if (userChoice == "scissors")
-                    {
-                        Console.WriteLine("The computer chose rock");
-                        Console.WriteLine("Sorry you lose,rock beats scissors");
-
-                    }
-
-                    else Console.WriteLine("Yoy must choose between rock, paper or scissors...");
-
-                }
-
-                else if (computerChoise == 3)
-                {
-                    if (userChoice == "rock")
-                    {
-                        Console.WriteLine("The computer chose scissors");
-                        Console.WriteLine("You win,rock beats scissors");
-
-                    }
-                    else if (userChoice == "paper")
-                    {
-                        Console.WriteLine("The computer chose rock");
-                        Console.WriteLine("You win,paper beats rock");
-
-                    }
-                    else if (userChoice == "scissors")
-                    {
-                        Console.WriteLine("The computer chose paper");
-                        Console.WriteLine("You win,scissors beat paper");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("You must choose rock,paper or scissors!");
-
-                    }
-
-                }
-
-                Console.ReadLine();
-            }
+           
         }
     }
 }
